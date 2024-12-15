1e5262863c43b53f467568d5443fc1afd6c365ef
Darya <Daramalasina72@gmail.com>
2024-12-16 02:21:12 +0300

    adding a line to the end

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -162,6 +162,7 @@ function isString(value) {
  * @param {string} value
  * @return {number}
  */
+
 function getCardId(value) {
   const suits = ['♣', '♦', '♥', '♠'];
   const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
